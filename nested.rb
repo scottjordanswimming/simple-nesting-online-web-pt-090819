5433c0a0d8f9c0ec1c5042781e391fc8c32833f5
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-28 23:25:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -2,9 +2,21 @@
 def hopper
 	programmer_hash = 
  		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
      }
 
-
+return programmer_hash[:grace_hopper]
 
 end
 
